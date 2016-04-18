5105d83f59d2772ee989db11c8be1cdd43a7116c
Yordan Georgiev <iordan340@gmail.com>
2016-04-18 18:42:51 +0300

    no message

--- js/angular-uploader.js
+++ js/angular-uploader.js
@@ -79,6 +79,7 @@
 				scope: {
 					uploadUrl: '<',
 					downloadUrl: '<',
+					deleteUrl: '<',
 					multiple: '<?',
 					maxFileSize: '<?',
 					initFiles: '<?',
@@ -92,98 +93,132 @@
 
 
 	// Controller definition ============================================
-	var UploaderCtrl = ['$scope', '$timeout', 'Upload', function($scope, $timeout, Upload) {
-		var vm = this;
+	var UploaderCtrl = [
+		'$scope',
+		'$timeout',
+		'$http',
+		'Upload',
+		function($scope, $timeout, $http, Upload) {
+			var vm = this;
 
-		// set defaults =================================================
-		vm.files = $scope.initFiles = angular.isDefined($scope.initFiles) ? $scope.initFiles : [];
-		vm.invalidFiles = [];
-		vm.uploadUrl = $scope.uploadUrl;
-		vm.downloadUrl = $scope.downloadUrl;
-		vm.multiple = $scope.multiple = angular.isDefined($scope.multiple) ? $scope.multiple : false;
-		vm.maxFileSize = $scope.maxFileSize = angular.isDefined($scope.maxFileSize) ? $scope.maxFileSize : '2GB';
-		vm.showUploadBtn = true;
-		vm.debug = $scope.debug = angular.isDefined($scope.debug) ? $scope.debug : false;
+			// set defaults =================================================
+			vm.files = $scope.initFiles = angular.isDefined($scope.initFiles) ? $scope.initFiles : [];
+			vm.invalidFiles = [];
+			vm.uploadUrl = $scope.uploadUrl;
+			vm.downloadUrl = $scope.downloadUrl;
+			vm.deleteUrl = $scope.deleteUrl;
+			vm.multiple = $scope.multiple = angular.isDefined($scope.multiple) ? $scope.multiple : false;
+			vm.maxFileSize = $scope.maxFileSize = angular.isDefined($scope.maxFileSize) ? $scope.maxFileSize : '2GB';
+			vm.showUploadBtn = true;
+			vm.debug = $scope.debug = angular.isDefined($scope.debug) ? $scope.debug : false;
 
-		// debug watcher ================================================
-		if(vm.debug) {
-			var watchValues = [
-				'multiple',
-				'uploadUrl',
-				'downloadUrl',
-				'maxFileSize'
-			];
+			// debug watcher ================================================
+			if(vm.debug) {
+				var watchValues = [
+					'multiple',
+					'uploadUrl',
+					'downloadUrl',
+					'maxFileSize'
+				];
 
-			$scope.$watchGroup(watchValues, function(newValues, oldValues) {
-				vm.multiple = $scope.$eval($scope.multiple);
-				vm.uploadUrl = newValues[1];
-				vm.downloadUrl = newValues[2];
-				vm.maxFileSize = newValues[3];
+				$scope.$watchGroup(watchValues, function(newValues, oldValues) {
+					vm.multiple = $scope.$eval($scope.multiple);
+					vm.uploadUrl = newValues[1];
+					vm.downloadUrl = newValues[2];
+					vm.maxFileSize = newValues[3];
 
-				if(newValues[0] !== oldValues[0]) {
-					vm.files = [];
-				}
-
-			});
-		}
-
-		// load / upload files ==========================================
-		vm.load = function($files, $invalidFiles) {
-			var tempDate = new Date().getTime();
-
-			vm.files = vm.files.concat($files);
-			vm.invalidFiles = $invalidFiles;
-
-			angular.forEach($files, function(file) {
-				var fileDate = new Date(tempDate++);
-
-				file.data = {
-					date: fileDate,
-					name: file.name,
-					size: file.size
-				};
-				file.done = false;
-				file.isVisible = true;
-				file.animated = false;
-				file.progress = 0;
-
-				file.upload = Upload.upload({
-					url: vm.uploadUrl,
-					method: 'POST',
-					file: file
-				}).progress(function(evt) {
-					file.progress = parseInt(100.0 * evt.loaded / evt.total, 10);
-				}).success(function(response) {
-					file.data = response[0];
-					file.done = true;
-					file.progress = 100;
-					file.downloadUrl = vm.downloadUrl.replace(':id', file.data.uuid);
-
-				}).error(function(response, status) {
-					if (status > 0) {
-						console.error(response);
+					if(newValues[0] !== oldValues[0]) {
+						vm.files = [];
 					}
+
 				});
-			});
-
-			vm.showUploadBtn = !(vm.multiple === false && $files.length > 0);
-		};
-
-		// unload files =================================================
-		vm.unload = function($index, file) {
-			if(!file.done) {
-				file.upload.abort();
 			}
 
-			file.animated = true;
+			// load / upload files ==========================================
+			vm.load = function($files, $invalidFiles) {
+				var tempDate = new Date().getTime();
 
-			$timeout(function() {
-				file.isVisible = false;
-				vm.files.splice($index, 1);
+				vm.files = vm.files.concat($files);
+				vm.invalidFiles = $invalidFiles;
 
-				vm.showUploadBtn = !(vm.multiple === false && vm.files.length > 0);
-			}, 600);
-		};
+				angular.forEach($files, function(file, index) {
+					var fileDate = new Date(tempDate++);
 
-	}];
+					file.data = {
+						date: fileDate,
+						name: file.name,
+						size: file.size
+					};
+					file.done = false;
+					file.isVisible = true;
+					file.animated = false;
+					file.progress = 0;
+
+					file.upload = Upload.upload({
+						url: vm.uploadUrl,
+						method: 'POST',
+						file: file
+					}).progress(function(evt) {
+						if(evt.loaded !== evt.total) {
+							file.progress = parseInt(100.0 * evt.loaded / evt.total, 10);
+						}
+
+					}).success(function(response) {
+						file.data = response;
+						file.done = true;
+						file.progress = 100;
+						file.downloadUrl = vm.downloadUrl.replace(':id', file.data.uuid);
+
+						$scope.$emit('file:uploaded', {file: file});
+
+					}).error(function(response, status) {
+						if (status > 0) {
+							vm.removeElement(index, file);
+
+							var invalidFile = {
+								name: file.name,
+								size: file.size,
+								$error: response
+							};
+
+							if(angular.isArray($invalidFiles)) {
+								$invalidFiles.push(invalidFile);
+							}
+							else {
+								$invalidFiles = [invalidFile];
+							}
+						}
+					});
+				});
+
+				vm.showUploadBtn = !(vm.multiple === false && $files.length > 0);
+			};
+
+			vm.removeElement = function($index, file) {
+				file.animated = true;
+
+				$timeout(function() {
+					file.isVisible = false;
+					vm.files.splice($index, 1);
+
+					vm.showUploadBtn = !(vm.multiple === false && vm.files.length > 0);
+				}, 600);
+			};
+
+			// unload files =================================================
+			vm.unload = function($index, file) {
+				if(!file.done) {
+					file.upload.abort();
+				}
+
+				//// todo remove it from server
+				//$http.delete('/currentUser')
+				//	.success(function(data) {
+				//		$scope.user = data.user;
+				//	});
+
+				vm.removeElement($index, file);
+			};
+
+		}];
 })();
